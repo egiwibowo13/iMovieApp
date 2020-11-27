1867e53a3d76bf6c704a49c21b6d58e204d8f150
Egi A Wibowo <egiwibowo13@gmail.com>
2020-11-27 09:48:00 +0700

    initial iMovieApp

---
+++ iMovieApp/iMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMovieAppApp.swift
+//  iMovieApp
+//
+//  Created by Egi Wibowo on 26/11/20.
+//
+
+import SwiftUI
+
+@main
+struct iMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
